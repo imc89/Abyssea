2f4105eef45fbc70698536557c74a4d50d9ced2a
Iñigo Montánchez <mci.m89@gmail.com>
2025-07-31 22:38:28 +0200

    feat: creatures number constant

--- src/game/creatures.js
+++ src/game/creatures.js
@@ -7,8 +7,8 @@ export const creatureData = [
         description: 'El pez linterna es conocido por su bioluminiscencia, que utiliza para atraer presas y compañeros en las profundidades oscuras del océano. Su órgano luminoso, el fotóforo, es una adaptación fascinante a la vida en la zona batipelágica.',
         funFact: 'Algunas especies de peces linterna tienen fotóforos en sus ojos que les permiten ver en la oscuridad, ¡como si tuvieran gafas de visión nocturna naturales!',
         scale: 1.0,
-        minDepth: 200,
-        maxDepth: 4000,
+        minDepth: 10,
+        maxDepth: 50,
         numInstances: 15,
     },
     {
@@ -22,9 +22,9 @@ export const creatureData = [
         lightColor: 'rgba(0, 200, 255, 0.9)',
         lightRadius: 8,
         lightOffsetY: -20,
-        minDepth: 200,
-        maxDepth: 4000,
-        numInstances: 8,
+        minDepth: 50,
+        maxDepth: 90,
+        numInstances: 2,
     },
     {
         id: 'calamarMesopelagico',
@@ -33,8 +33,8 @@ export const creatureData = [
         description: 'Los calamares mesopelágicos son depredadores ágiles que habitan en la zona crepuscular del océano. Muchos tienen fotóforos para camuflarse o atraer presas, y son una parte vital de la cadena alimentaria en estas profundidades.',
         funFact: 'Algunos calamares de las profundidades pueden expulsar una "tinta" bioluminiscente en lugar de tinta oscura, para confundir a los atacantes en la oscuridad.',
         scale: 0.85,
-        minDepth: 200,
-        maxDepth: 1000,
-        numInstances: 5,
+        minDepth: 90,
+        maxDepth: 130,
+        numInstances: 3,
     }
 ];
