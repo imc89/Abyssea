80b1e8748643285a541f0888cb7c0ed71054205f
Iñigo Montánchez <mci.m89@gmail.com>
2025-08-01 20:57:30 +0200

    feat: add constants

--- src/game/constants.js
+++ src/game/constants.js
@@ -7,17 +7,17 @@ export const SPOTLIGHT_CHARGE_RATE = 0.03; // Tasa de recarga de batería del fo
 // --- Spotlight Position and Shape ---
 // These constants control the position and shape of the submarine's spotlight.
 // The origin of the light is relative to the submarine's dimensions.
-export const SPOTLIGHT_HORIZONTAL_OFFSET = 0.8; // Horizontal position of the light source on the submarine. 0.8 means 80% from the left edge.
+export const SPOTLIGHT_HORIZONTAL_OFFSET = 1; // Horizontal position of the light source on the submarine. 0.8 means 80% from the left edge.
 export const SPOTLIGHT_VERTICAL_OFFSET = 0.6; // Vertical position of the light source on the submarine. 0.6 means 60% from the top edge.
 export const SPOTLIGHT_LENGTH = 350; // The length of the spotlight beam in pixels.
 export const SPOTLIGHT_WIDTH_AT_END = 150; // The width of the spotlight beam at its end in pixels.
-export const AMBIENT_LIGHT_RADIUS = 80; // Radio de la luz ambiental.
+export const AMBIENT_LIGHT_RADIUS = 100; // Radio de la luz ambiental.
 export const AMBIENT_LIGHT_MAX_OPACITY = 0.5; // Opacidad máxima de la luz ambiental.
 export const SUBMARINE_IMAGE_URL = 'https://i.ibb.co/dwPyHB8L/submarine.gif'; // URL de la imagen del submarino.
 export const SUBMARINE_STATIC_IMAGE_URL = 'https://i.ibb.co/ympgPXB0/submarine-static.png';
 export const SUBMARINE_BASE_WIDTH = 120;
 export const SUBMARINE_BASE_HEIGHT = 60;
-export const SUBMARINE_SCALE_FACTOR = 1.5;
+export const SUBMARINE_SCALE_FACTOR = 1.3;
 export const SUBMARINE_HORIZONTAL_SPEED = 5;
 export const SUBMARINE_VERTICAL_SPEED = 3;
 export const PARTICLE_DENSITY_FACTOR = 1.0;
